9a7efac5e9cd89b791a4390f2a5f06bf23ee9d06
Glitch (tart-cemetery) <none>
2019-06-11 16:13:48 +0000

    🌸🎃 Checkpoint ./README.md:86128/1930 ./lamp-start.sh:86128/40 ./controllers/index.php:86128/214 ./views/header.html:86128/663

--- controllers/index.php
+++ controllers/index.php
@@ -1,10 +1,13 @@
 <?php
 include_once "include/util.php";
-include_once "models/post.php";
-include_once "controllers/post.php";
 
 function get_index() {
-    get_list();
+  renderTemplate(
+    "views/index.php",
+    array(
+      'title' => 'PHP Forms Examples'
+    )
+  );
 }
 
 ?>
--- controllers/reset.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include_once "include/util.php";
-
-  function post_index($params) {
-    $output = `sqlite3 blog.db3 < blog.sql`;
-    redirectRelative("index");
-  }
-?>
