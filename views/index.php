2c2db9116a8ba7a90ce0fe3df540b41d436b6e72
Glitch (tart-cemetery) <none>
2019-06-11 19:57:52 +0000

    🏅🐓 Checkpoint ./views/index.php:86128/9935 ./views/process.php:86128/1193

--- views/index.php
+++ views/index.php
@@ -1,10 +1,65 @@
 %% views/header.html %%
 
+
 <div class="row">
+  <div class="col-lg-12">
+
+  <!-- contact us form example with textarea and select -->
+                
+<div class="row mt-4">
   <div class="col-lg-12">
     <div class="card">
       <div class="card-header">
-        <span class="h3">Create your account</span>
+        <div class="h3">Contact us!</div>
+        <div><small>This form does no validation and submits its data to top-level variables. Also if there are errors, no data is retained in the form (all must be retyped).</small></div>
+      </div>
+      
+      <div class="card-body">
+        <form action="@@process/contact@@" method="post">
+          <div class="form-row">
+            <div class="col">
+              <label for="subject">Subject</label>
+              <input type="text" class="form-control" id="subject" name="subject" placeholder="Enter subject" required>
+            </div>
+          </div>
+          <div class="form-row mt-4">
+            <div class="col">
+              <label for="email3">Email address</label>
+              <input type="email" class="form-control" id="email3" name="email3" placeholder="Enter email address" required>
+            </div>
+            <div class="col">
+              <label for="product">State</label>
+              <select name="product" id="product" class="form-control">
+                <option value="invalid" selected>Choose product...</option>
+                <option value="books">Books</option>
+                <option value="music">Music</option>
+                <option value="magazine">Magazines</option>
+                <option value="videos">Videos</option>
+              </select>
+            </div>
+          </div>
+                
+          <div class="form-row mt-4">
+            <div class="col">
+              <label for="message">Content</label>
+              <textarea class="form-control" id="message" name="message" placeholder="Enter message" rows="12"></textarea>
+            </div>
+          </div>
+          <div class="form-row mt-4 float-right">
+            <div class="btn-toolbar align-middle">
+              <button type="submit" class="btn btn-primary mr-1 d-flex justify-content-center align-content-between"><span class="material-icons">send</span>&nbsp;Submit</button>
+              <button class="btn btn-secondary mr-1 d-flex justify-content-center align-content-between" onclick="get('@@index@@')"><span class="material-icons">cancel</span>&nbsp;Cancel</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+
+  
+    <div class="card mt-4">
+      <div class="card-header">
+        <div class="h3">Create your account</div>
+        <div><small>This form does server-side validation (some small client-side validation as well, but server side must also be performed). It submits its data to an array. Also if there are errors, data is retained in the form.</small></div>
       </div>
 [[ if (isset($accountErrors)): ]]
       <div class="alert alert-danger mb-0" role="alert">
@@ -98,64 +153,6 @@
 
                 
                 
-<!-- contact us form example with textarea and select -->
-                
-<div class="row mt-4">
-  <div class="col-lg-12">
-    <div class="card">
-      <div class="card-header">
-        <span class="h3">Contact us!</span>
-      </div>
-[[ if (isset($contactErrors)): ]]
-      <div class="alert alert-danger mb-0" role="alert">
-        Please fix the following errors
-        <ul>
-[[ foreach ($contactErrors as $key => $error): ]]
-          <li>{{$error}}</li>
-[[ endforeach; ]]
-        </ul>
-      </div>
-[[ endif; ]]
-      <div class="card-body">
-        <form action="@@process/contact@@" method="post">
-          <div class="form-row">
-            <div class="col">
-              <label for="subject">Subject</label>
-              <input type="text" class="form-control" id="subject" name="form[subject]" placeholder="Enter subject" required>
-            </div>
-          </div>
-          <div class="form-row mt-4">
-            <div class="col">
-              <label for="email3">Email address</label>
-              <input type="email" class="form-control" id="email3" name="form[email3]" placeholder="Enter email address" required>
-            </div>
-            <div class="col">
-              <label for="product">State</label>
-              <select name="form[product]" id="product" class="form-control">
-                <option value="invalid" selected>Choose product...</option>
-                <option value="books">Books</option>
-                <option value="music">Music</option>
-                <option value="magazine">Magazines</option>
-                <option value="videos">Videos</option>
-              </select>
-            </div>
-          </div>
-                
-          <div class="form-row mt-4">
-            <div class="col">
-              <label for="message">Content</label>
-              <textarea class="form-control" id="message" name="form[message]" placeholder="Enter message" rows="12"></textarea>
-            </div>
-          </div>
-          <div class="form-row mt-4 float-right">
-            <div class="btn-toolbar align-middle">
-              <button type="submit" class="btn btn-primary mr-1 d-flex justify-content-center align-content-between"><span class="material-icons">send</span>&nbsp;Submit</button>
-              <button class="btn btn-secondary mr-1 d-flex justify-content-center align-content-between" onclick="get('@@index@@')"><span class="material-icons">cancel</span>&nbsp;Cancel</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
   </div>
 </div>
 
--- views/process.php
+++ views/process.php
@@ -6,4 +6,14 @@
   </div>
 </div>
 
+<div class="row mt-4">
+  <div class="col-lg-12">
+    <div class="form-group">
+      <div class="btn-toolbar align-middle">
+        <button class="btn btn-secondary mr-1 d-flex justify-content-center align-content-between" onclick="return get('@@index@@')"><span class="material-icons">arrow_back</span>&nbsp;Back</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 %% views/footer.html %% 
